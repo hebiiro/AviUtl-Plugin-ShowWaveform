7b2b06de7d2669da47ff935185d2fab9c0ba19f7
hebiiro <io.hebiiro@gmail.com>
2025-06-24 18:33:54 +0900

    🩹動画ファイルと連携していると音声を取得できない場合がある問題に対応

--- ShowWaveform/ItemCache.cpp
+++ ShowWaveform/ItemCache.cpp
@@ -159,6 +159,8 @@ AudioParamsPtr ItemCacheManager::getAudioParams(ExEdit::Object* object)
 	ExEdit::LayerSetting* layer = theApp.m_auin.GetLayerSetting(object->layer_set);
 	params->layerFlag = (uint32_t)layer->flag;
 
+	auto fps = (float)theApp.m_fi.video_rate / theApp.m_fi.video_scale;
+
 	// 動画ファイルと連携している場合は動画ファイルから再生位置を取得します。
 	if (object->check_value[1])
 	{
@@ -170,8 +172,10 @@ AudioParamsPtr ItemCacheManager::getAudioParams(ExEdit::Object* object)
 			// 拡張データを取得する。
 			auto exdata = (ExEdit::Exdata::efMovieFile*)theApp.m_auin.GetExdata(object2, 0);
 
+			auto video_fps = (float)exdata->video_rate / exdata->video_scale;
+
 			copyFileName(params->fileName, sizeof(params->fileName), exdata->file);
-			params->playBegin = object2->track_value_left[playBeginIndex];
+			params->playBegin = (int32_t)(object2->track_value_left[playBeginIndex] * fps / video_fps);
 			params->playSpeed = object2->track_value_left[playSpeedIndex] / 1000.0f;
 		}
 	}
@@ -182,7 +186,7 @@ AudioParamsPtr ItemCacheManager::getAudioParams(ExEdit::Object* object)
 		auto exdata = (ExEdit::Exdata::efAudioFile*)theApp.m_auin.GetExdata(object, 0);
 
 		copyFileName(params->fileName, sizeof(params->fileName), exdata->file);
-		params->playBegin = object->track_value_left[playBeginIndex] * theApp.m_fi.video_rate / theApp.m_fi.video_scale / 100;
+		params->playBegin = (int32_t)(object->track_value_left[playBeginIndex] * fps / 100.0f);
 		params->playSpeed = object->track_value_left[playSpeedIndex] / 1000.0f;
 	}
 
@@ -218,6 +222,55 @@ void ItemCacheManager::copyFileName(LPSTR dst, size_t size, LPCSTR src)
 		else
 			dst[i] = tolower(dst[i]);
 	}
+}
+
+//
+// 音声ファイルアイテムと連携している動画ファイルアイテムを返します。
+//
+ExEdit::Object* ItemCacheManager::get_movie_file_item(ExEdit::Object* object)
+{
+	auto result = (ExEdit::Object*)nullptr;
+	auto distance = 100;
+
+	// 同じ開始位置で最も近いレイヤーにある動画アイテムを探します。
+	auto c = theApp.m_auin.GetCurrentSceneObjectCount();
+	for (decltype(c) i = 0; i < c; i++)
+	{
+		auto object2 = theApp.m_auin.GetSortedObject(i);
+
+		// シーンが異なるアイテムは無視します。
+		if (object2->scene_set != object->scene_set) continue;
+
+		// 開始位置が異なるアイテムは無視します。
+		if (object2->frame_begin != object->frame_begin) continue;
+
+		// レイヤー位置が下のアイテムは無視します。
+		if (object2->layer_set >= object->layer_set) continue;
+
+		// レイヤー位置が遠い場合は無視します。
+		auto distance2 = object->layer_set - object2->layer_set;
+		if (distance2 > distance) continue;
+#if 0
+		// 音声ファイルアイテムの場合は
+		if (object2->filter_param[0].id == c_audio_file_filter_id)
+		{
+			// この音声ファイルアイテムより下にある動画ファイルアイテムのみを連携対象とみなします。
+			result = nullptr;
+			distance = distance2;
+		}
+		else
+#endif
+		{
+			// 動画ファイル(id=0)以外のアイテムは無視します。
+			if (object2->filter_param[0].id != 0) continue;
+
+			// この動画ファイルアイテムを連携対象とします。
+			result = object2;
+			distance = distance2;
+		}
+	}
+
+	return result;
 }
 
 //---------------------------------------------------------------------
--- ShowWaveform/ItemCache.h
+++ ShowWaveform/ItemCache.h
@@ -21,51 +21,7 @@ struct ItemCacheManager {
 	//
 	// 音声ファイルアイテムと連携している動画ファイルアイテムを返します。
 	//
-	inline static ExEdit::Object* get_movie_file_item(ExEdit::Object* object)
-	{
-		auto result = (ExEdit::Object*)nullptr;
-		auto distance = 100;
-
-		// 同じ開始位置で最も近いレイヤーにある動画アイテムを探します。
-		auto c = theApp.m_auin.GetCurrentSceneObjectCount();
-		for (decltype(c) i = 0; i < c; i++)
-		{
-			auto object2 = theApp.m_auin.GetSortedObject(i);
-
-			// シーンが異なるアイテムは無視します。
-			if (object2->scene_set != object->scene_set) continue;
-
-			// 開始位置が異なるアイテムは無視します。
-			if (object2->frame_begin != object->frame_begin) continue;
-
-			// レイヤー位置が下のアイテムは無視します。
-			if (object2->layer_set >= object->layer_set) continue;
-
-			// レイヤー位置が遠い場合は無視します。
-			auto distance2 = object->layer_set - object2->layer_set;
-			if (distance2 > distance) continue;
-#if 0
-			// 音声ファイルアイテムの場合は
-			if (object2->filter_param[0].id == c_audio_file_filter_id)
-			{
-				// この音声ファイルアイテムより下にある動画ファイルアイテムのみを連携対象とみなします。
-				result = nullptr;
-				distance = distance2;
-			}
-			else
-#endif
-			{
-				// 動画ファイル(id=0)以外のアイテムは無視します。
-				if (object2->filter_param[0].id != 0) continue;
-
-				// この動画ファイルアイテムを連携対象とします。
-				result = object2;
-				distance = distance2;
-			}
-		}
-
-		return result;
-	}
+	static ExEdit::Object* get_movie_file_item(ExEdit::Object* object);
 };
 
 //---------------------------------------------------------------------
